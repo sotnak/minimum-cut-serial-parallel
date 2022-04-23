46e4f9540d7332f08a409e937cf95ee5161ec297
Pavel Antos <antospa2@cvut.cz>
2022-04-23 14:58:39 +0200

    job and custom output

--- src/ArgGetter.cpp
+++ src/ArgGetter.cpp
@@ -21,3 +21,16 @@ const char *ArgGetter::getSourcePath(int argc, char **argv) {
 
     throw invalid_argument("--source not found");
 }
+
+const char *ArgGetter::getOutputPath(int argc, char **argv) {
+    for(int i=0; i<argc; i++) {
+        if (strcmp(argv[i], "--out") == 0) {
+            if (i + 1 >= argc) {
+                throw invalid_argument("file not specified");
+            }
+            return argv[i + 1];
+        }
+    }
+
+    throw invalid_argument("--out not found");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -36,14 +36,13 @@ ifstream getInput(const char* path){
 
 ofstream getOutput(const char* path){
     const string sPath = string(path);
-    const string outPath = "OUT/" + sPath.substr(sPath.find('/')+1).substr(sPath.find('/')+1);
-    const string folders = outPath.substr(0,outPath.find_last_of('/'));
+    const string folders = sPath.substr(0,sPath.find_last_of('/'));
 
     if(!fs::is_directory(folders))
         fs::create_directories(folders);
 
     ofstream output;
-    output.open(outPath);
+    output.open(sPath);
     if(output.fail())
         throw exception();
     return output;
@@ -61,9 +60,10 @@ vector<string> splitString(const string& str, char delimiter = ' '){
 
 void master(int argc, char *argv[]){
     const char* path = ArgGetter::getSourcePath(argc, argv);
+    const char* outPath = ArgGetter::getOutputPath(argc, argv);
 
     ifstream input = getInput(path);
-    ofstream output = getOutput(path);
+    ofstream output = getOutput(outPath);
     auto solver = new MasterBBSolver();
 
     chrono::steady_clock::time_point t1;
