0ea60253cf2557d8d4c16045c9c44baa80a7b7ce
Pavel Antos <antospa2@cvut.cz>
2022-04-23 14:59:19 +0200

    job and custom output

--- src/ArgGetter.cpp
+++ src/ArgGetter.cpp
@@ -41,3 +41,16 @@ int ArgGetter::getThreadCount(int argc, char **argv) {
     return -1;
 }
 
+const char *ArgGetter::getOutputPath(int argc, char **argv) {
+    for(int i=0; i<argc; i++) {
+        if (strcmp(argv[i], "--out") == 0) {
+            if (i + 1 >= argc) {
+                throw invalid_argument("file not specified");
+            }
+            return argv[i + 1];
+        }
+    }
+
+    throw invalid_argument("--out not found");
+}
+
