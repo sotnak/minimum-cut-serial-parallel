a14123e318873ffecf684e5ac92622e667955f72
Pavel Antoš <antospa2@fit.cvut.cz>
2022-04-24 13:32:44 +0200

    Update serial_job.sh

--- serial_job.sh
+++ serial_job.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 
 #  ===========================================================================
 # |                                                                           |
@@ -13,18 +13,65 @@
 #  ===========================================================================
 
 # Request Bourne shell as shell for job
-#$ -S /bin/sh
+$ -S /bin/sh
 
 # Execute the job from the current working directory.
-#$ -cwd
+$ -cwd
 
 # Defines  or  redefines  the  path used for the standard error stream of the job.
-#$ -e .
+$ -e ./err.log
 
 # The path used for the standard output stream of the job.
-#$ -o .
+$ -o ./out.log
 
 # Do not change.
-#$ -pe ompi 1
+$ -pe ompi 1
 
-./my_sequential_application arg1 arg2 ...
+echo 'graf_20_17_t2:'
+./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t2.txt --threads 2
+
+echo 'graf_30_10_t2:'
+./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t2.txt --threads 2
+
+echo 'graf_30_20_t2:'
+./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t2.txt --threads 2
+
+
+echo 'graf_20_17_t4:'
+./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t4.txt --threads 4
+
+echo 'graf_30_10_t4:'
+./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t4.txt --threads 4
+
+echo 'graf_30_20_t4:'
+./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t4.txt --threads 4
+
+
+echo 'graf_20_17_t8:'
+./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t8.txt --threads 8
+
+echo 'graf_30_10_t8:'
+./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t8.txt --threads 8
+
+echo 'graf_30_20_t8:'
+./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t8.txt --threads 8
+
+
+echo 'graf_20_17_t16:'
+./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t16.txt --threads 16
+
+echo 'graf_30_10_t16:'
+./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t16.txt --threads 16
+
+echo 'graf_30_20_t16:'
+./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t16.txt --threads 16
+
+
+echo 'graf_20_17_t20:'
+./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t20.txt --threads 20
+
+echo 'graf_30_10_t20:'
+./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t20.txt --threads 20
+
+echo 'graf_30_20_t20:'
+./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t20.txt --threads 20
