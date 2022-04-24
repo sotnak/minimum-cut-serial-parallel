96b80cf6bba17695d4bbf72cf3e99d31cb11c8a1
Pavel Antos <antospa2@star.star.fit.cvut.cz>
2022-04-24 13:06:37 +0200

    job script

--- serial_job.sh
+++ serial_job.sh
@@ -1,52 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-make
- 
- echo 'graf_20_17_t2:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t2.txt --threads 2
+#  ===========================================================================
+# |                                                                           |
+# |             COMMAND FILE FOR SUBMITTING SGE JOBS                          |
+# |                                                                           |
+# |                                                                           |
+# | SGE keyword statements begin with #$                                      |
+# |                                                                           |
+# | Comments begin with #                                                     |
+# | Any line whose first non-blank character is a pound sign (#)              |
+# | and is not a SGE keyword statement is regarded as a comment.              |
+#  ===========================================================================
 
-echo 'graf_30_10_t2:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t2.txt --threads 2
+# Request Bourne shell as shell for job
+#$ -S /bin/sh
 
-echo 'graf_30_20_t2:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t2.txt --threads 2
+# Execute the job from the current working directory.
+#$ -cwd
 
+# Defines  or  redefines  the  path used for the standard error stream of the job.
+#$ -e .
 
-echo 'graf_20_17_t4:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t4.txt --threads 4
+# The path used for the standard output stream of the job.
+#$ -o .
 
-echo 'graf_30_10_t4:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t4.txt --threads 4
+# Do not change.
+#$ -pe ompi 1
 
-echo 'graf_30_20_t4:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t4.txt --threads 4
-
-
-echo 'graf_20_17_t8:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t8.txt --threads 8
-
-echo 'graf_30_10_t8:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t8.txt --threads 8
-
-echo 'graf_30_20_t8:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t8.txt --threads 8
-
-
-echo 'graf_20_17_t16:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t16.txt --threads 16
-
-echo 'graf_30_10_t16:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t16.txt --threads 16
-
-echo 'graf_30_20_t16:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t16.txt --threads 16
-
-
-echo 'graf_20_17_t20:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17_t20.txt --threads 20
-
-echo 'graf_30_10_t20:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10_t20.txt --threads 20
-
-echo 'graf_30_20_t20:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20_t20.txt --threads 20
+./my_sequential_application arg1 arg2 ...
