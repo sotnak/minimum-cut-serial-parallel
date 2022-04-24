524345f07c8fb9873d3c30530864f512a2c73aec
Pavel Antos <antospa2@star.star.fit.cvut.cz>
2022-04-24 13:07:30 +0200

    job script

--- serial_job.sh
+++ serial_job.sh
@@ -1,12 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-make
- 
-echo 'graf_20_17:'
-./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17.txt
+#  ===========================================================================
+# |                                                                           |
+# |             COMMAND FILE FOR SUBMITTING SGE JOBS                          |
+# |                                                                           |
+# |                                                                           |
+# | SGE keyword statements begin with #$                                      |
+# |                                                                           |
+# | Comments begin with #                                                     |
+# | Any line whose first non-blank character is a pound sign (#)              |
+# | and is not a SGE keyword statement is regarded as a comment.              |
+#  ===========================================================================
 
-echo 'graf_30_10:'
-./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10.txt
+# Request Bourne shell as shell for job
+#$ -S /bin/sh
 
-echo 'graf_30_20:'
-./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20.txt
+# Execute the job from the current working directory.
+#$ -cwd
+
+# Defines  or  redefines  the  path used for the standard error stream of the job.
+#$ -e .
+
+# The path used for the standard output stream of the job.
+#$ -o .
+
+# Do not change.
+#$ -pe ompi 1
+
+./my_sequential_application arg1 arg2 ...
