ed5c7d7e235aae80b71a65c94c71c05b4983a3aa
Pavel Antoš <antospa2@fit.cvut.cz>
2022-04-30 13:45:55 +0200

    graf 40

--- parallel_job.sh
+++ parallel_job.sh
@@ -27,12 +27,21 @@
 # Do not change.
 #$ -pe ompi 1
 
-echo 'graf_20_17:'
-mpirun --bind-to none -np 4 ./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17.txt
+# echo 'graf_20_17:'
+# mpirun --bind-to none -np 4 ./app --source ./IN/graf_20_17.txt --out ./OUT/graf_20_17.txt
 
-echo 'graf_30_10:'
-mpirun --bind-to none -np 4 ./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10.txt
+# echo 'graf_30_10:'
+# mpirun --bind-to none -np 4 ./app --source ./IN/graf_30_10.txt --out ./OUT/graf_30_10.txt
 
-echo 'graf_30_20:'
-mpirun --bind-to none -np 4 ./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20.txt
+# echo 'graf_30_20:'
+# mpirun --bind-to none -np 4 ./app --source ./IN/graf_30_20.txt --out ./OUT/graf_30_20.txt
+
+echo 'graf_40_8:'
+mpirun --bind-to none -np 4 ./app --source ./IN/graf_40_8.txt --out ./OUT/graf_40_8.txt
+
+echo 'graf_40_15:'
+mpirun --bind-to none -np 4 ./app --source ./IN/graf_40_15.txt --out ./OUT/graf_40_15.txt
+
+echo 'graf_40_25:'
+mpirun --bind-to none -np 4 ./app --source ./IN/graf_40_25.txt --out ./OUT/graf_40_25.txt
 
